8e9097307258fd1990bb5d40532cabe78dfb4a9e
Roshan Ghimire <roshan13ghimire@gmail.com>
2026-07-25 15:59:01 -0400

    configure for frontend.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,11 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "employee-onboarding-backend-traj.onrender.com",
-]
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
